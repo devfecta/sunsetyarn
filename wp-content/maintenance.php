7fe7612f3bb1c3444fbf3067a03c7a1ec6f6541a
root <root@ubuntu20-04vm.localdomain>
2020-07-16 17:01:05 -0500

    Enabled maintenance mode

---
+++ wp-content/maintenance.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sunset Yarn, LLC</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
+    </style>
+</head>
+<body>
+<section>
+    <div style="text-align: center;">
+        <img src="./wp-content/uploads/2020/07/Sunset-Yarn-V.png" alt="Sunset Yarn, LLC" style="clear: both; margin: 1em;" /><br />
+        <strong style="font-size: 3em;">Opening in October 2020</strong>
+        <h2>For More Information Contact Us At:</h2>
+        <p><strong>Mailing Address: </strong>3742 Speedway Rd Madison, WI 53705</p>
+        <p><strong>e-Mail: </strong><a href="mailto:lynette@sunsetyarn.com">lynette@sunsetyarn.com</a></p>
+        <p><strong>Phone: </strong>(608) 957-6008</p>
+    </div>
+</section>
+</body>
+</html>
