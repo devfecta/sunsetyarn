2a0315daa6a1be5780747236a7fb670e115d6d39
root <root@ubuntu20-04vm.localdomain>
2020-07-25 14:37:04 -0500

    Fixing functions file

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -28,7 +28,7 @@ function my_login_logo_url() {
     return home_url();
 }
 
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+add_filter( 'login_headertext', 'my_login_logo_url_title' );
 function my_login_logo_url_title() {
     return 'Sunset Yarn, LLC';
 }
